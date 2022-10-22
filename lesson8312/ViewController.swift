92a9dc6b41220c754546f6b5c409c28a981e3f97
avedevios <106875633+avedevios@users.noreply.github.com>
2022-10-22 20:17:51 +0600

    Initial Commit

---
+++ lesson8312/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson8312
+//
+//  Created by ake11a on 2022-10-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
